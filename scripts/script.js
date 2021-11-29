03de5f5caf3663fc1335d31fec0778182ab15540
Ivan Hamílton Sgarbi <ivan.dk666@hotmail.com>
2021-11-28 23:56:26 -0300

    Novo função de URL dos episódios, Vue de produção

--- scripts/script.js
+++ scripts/script.js
@@ -27,12 +27,17 @@ function xmlToDoc(xml) {
         mp3 = items[i].querySelector("enclosure").getAttribute("url");
         desc = items[i].getElementsByTagName("itunes:summary")[0].childNodes[0].textContent;
         img = items[i].getElementsByTagName("itunes:image")[0].getAttribute("href");
+        season = items[i].getElementsByTagName("itunes:season")[0].textContent;
+        number = items[i].getElementsByTagName("itunes:episode")[0] ? items[i].getElementsByTagName("itunes:episode")[0].textContent : 0;
 
         episodes.push({
+            id: i,
             title : title,
             mp3 : mp3,
             desc : desc,
-            img : img
+            img : img,
+            season: season,
+            number: number
         });        
     }
     console.log(episodes);
@@ -40,13 +45,11 @@ function xmlToDoc(xml) {
     app = new Vue(
         {
             el:"#content",
-            data: {episodes: episodes}
+            data: {episodes: episodes, selectedEpisode: null}
         }
     );
 
     clearLoading();
-
-    
 }
 function clearLoading(){
     var container = document.querySelector(".loading-content");
@@ -71,6 +74,8 @@ function clearLoading(){
         function(){
             document.querySelector(".episodes-container").classList.remove("hidden");
             container.setAttribute("style", "display:none;");
+
+            searchEpisode()
         },
         2100
     );
@@ -87,4 +92,35 @@ function setBanner(){
 
 document.body.onresize = function(evt){
     setBanner();
+}
+function clearEpisode() {
+    app.selectedEpisode = null;
+    //window.location.hash = '';
+}
+function setEpisode(event) {
+    var id = event.getAttribute("id");
+    var episode = app.episodes[id];
+    console.log("Clicou no episódio:", event);
+    console.log(app);
+    window.location.hash = "episodio="+episode.number+"temporada="+episode.season;
+    app.selectedEpisode = episode;
+}
+
+function searchEpisode(){
+    if(window.location.hash == '' || !window.location.hash){
+        return;
+    }
+    var episode,season,hash,data;
+    hash = window.location.hash
+    episode = hash.substring(hash.indexOf("episodio=") + 9, hash.indexOf("temporada"));
+    season = hash.substring(hash.indexOf("temporada=") + 10);
+    console.log("Temporada: ", season, "\nEpisodio: ", episode);
+
+    for (var i = 0; i < app.episodes.length; i++) {
+        data = app.episodes[i];
+        if(data.season == season && data.number == episode){
+            app.selectedEpisode = data;
+            return;
+        }
+    }
 }
